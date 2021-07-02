4514834d5b43cec0e9222a0978b35cd30250affa
Nathan <n.gusky88@gmail.com>
2021-07-02 14:12:49 -0600

    frontend

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log('server started')
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+io.on('connection', socket => {
+     console.log('connection successful')
+     socket.on('message', payload => {
+          console.log('Message received on server side: ', payload)
+          io.emit('message', payload)
+     })
+})
+
+server.listen(4000, () => {
+     console.log('listening to port: 4000')
+})
